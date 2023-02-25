75d02792fdc6eab7a955b905ac021bdf5e6de5b4
Marco <114459798+marcodillanes@users.noreply.github.com>
2023-02-24 20:17:22 -0800

    part 7 push

--- controllers/places.js
+++ controllers/places.js
@@ -30,21 +30,33 @@ router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
-router.post('/', (req, res) => {
-  console.log(req.body)
-  if (!req.body.pic) {
-    // Default image if one is not provided
-    req.body.pic = 'http://placekitten.com/400/400'
+router.put('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
   }
-  if (!req.body.city) {
-    req.body.city = 'Anytown'
+  else if (!places[id]) {
+      res.render('error404')
   }
-  if (!req.body.state) {
-    req.body.state = 'USA'
+  else {
+      // Dig into req.body and make sure data is valid
+      if (!req.body.pic) {
+          // Default image if one is not provided
+          req.body.pic = 'http://placekitten.com/400/400'
+      }
+      if (!req.body.city) {
+          req.body.city = 'Anytown'
+      }
+      if (!req.body.state) {
+          req.body.state = 'USA'
+      }
+
+      // Save the new data into places[id]
+      places[id] = req.body
+      res.redirect(`/places/${id}`)
   }
-  places.push(req.body)
-  res.redirect('/places')
 })
+
 
 
 
